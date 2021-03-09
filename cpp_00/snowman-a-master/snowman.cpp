27189a937db25da2e6abea2b7e3c1275b1396209
Liroy <zaqwedsxc2@gmail.com>
2021-03-09 18:30:43 +0200

    Commit no 3

--- cpp_00/snowman-a-master/snowman.cpp
+++ cpp_00/snowman-a-master/snowman.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <iostream>
 #include "snowman.hpp"
 
@@ -9,7 +10,35 @@ namespace ariel{
         for (int i = number.length(); i >= 0; i--){
             if (number[i]>4 || number[i]<1) throw invalid_argument("Illegal Input!");
         }
+        const int H = 0; //Hat
+        const int N = 1; //Nose
+        const int L = 2; //Left eye
+        const int R = 3; //Right eye
+        const int X = 4; //Left arm
+        const int Y = 5; //Right arm
+        const int T = 6; //Torso
+        const int B = 7; //Base
         string snowman="";
-        snowman.append(cloth::)
+        snowman.insert(cloth::hats[number[H]);
+        snowman.insert("\n");
+        snowman.insert(cloth::leftArm[number[X]]);
+        snowman.insert("(");
+        snowman.insert(cloth::eyes[number[L]);
+        snowman.insert(cloth::nose[number[N]);
+        snowman.insert(cloth::eyes[number[R]);
+        snowman.insert(")");
+        snowman.insert(cloth::rightArm[number[Y]]);
+        snowman.insert("\n");
+        answer.append(clothing::leftArm[number[X]]);
+        snowman.insert("(");
+        answer.append(clothing::belly[number[T]]);
+        snowman.insert(")");
+        snowman.insert(cloth::rightArm[number[Y]]);
+        answer.append(clothing::Base[number[B]]);
+
+        return snowman;
+
+
+
     }
 }
--- cpp_00/snowman-a-master/snowman.hpp
+++ cpp_00/snowman-a-master/snowman.hpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 namespace cloth{
-    const std::array<string,4> hat {"_===_"," ___\n.....","  _\n /_\\"," ___\n(_*_)"};
-    const std::array<string,4> nose {",",".","_"," "};
-    const std::array<string,4> eye {".","o","O","-"};
-    const std::array<string,4> leftArm {"<","\\","/",""};
-    const std::array<string,4> rightArm {">","/","\\",""};
-    const std::array<string,4> torso {" : ","] [","> <","   "};
-    const std::array<string,4> base {" : ","\" \"","___","   "};
+    const string hats[4] = {"_===_"," ___\n.....","  _\n /_\\"," ___\n(_*_)"};
+    const string nose[4] = {",",".","_"," "};
+    const string eyes[4] = {".","o","O","-"};
+    const string leftArm[4] = {"<","\\","/",""};
+    const string rightArm[4] = {">","/","\\",""};
+    const string torso[4] = {" : ","] [","> <","   "};
+    const string base[4] = {" : ","\" \"","___","   "};
 };
 
 namespace ariel{
